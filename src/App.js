5fdffe34d1a396ae7e1d3a4229acefe61cfc8dd3
machlarz <mimac1982@gmail.com>
2025-05-10 14:43:05 +0200

    Reaktywnosc

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let email = 'fracz@agh.edu.pl';
+    const [email, setEmail] = useState('fracz@agh.edu.pl');
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setEmail(event.target.value);
+    }
+
+    let message;
+    if (email.length < 5) {
+        message = 'Twój email jest za krótki!';
+    } else if (email.length < 15) {
+        message = 'Twój adres e-mail jest w sam raz.';
+    } else {
+        message = 'Twój adres e-mail jest stanowczo za długi.';
     }
 
     return (
-
         <div>
-
             <h1>System do zapisów na zajęcia</h1>
+            <div>{message}</div>
             <h2>Twój e-mail to {email}</h2>
-            <input type="text" onChange={handleChange}/>
+            <input type="text" value={email} onChange={handleChange}/>
         </div>
-
     );
 }
 
